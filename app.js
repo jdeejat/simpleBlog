58063b50d1f56bdcee88074ce3b8e914be0f20c5
Niki <jdeejat@gmail.com>
2022-07-19 19:09:51 +0200

    updated listen port

--- app.js
+++ app.js
@@ -122,7 +122,11 @@ app.get('/posts/:postName', function (req, res) {
   });
 });
 
-//listen for port 3000
-app.listen(3000, function () {
+// SERVER CONFIG
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3000;
+}
+app.listen(port, function () {
   console.log('Server started on port 3000');
 });
